b05bd14ca4c58d641187cd3c399d94fad11a1be8
yjr2268152113 <2268152113@qq.com>
2019-06-18 00:17:39 +0800

    6.18

--- src/components/TSstudent.vue
+++ src/components/TSstudent.vue
@@ -1,14 +1,53 @@
-<template>
-  <div class="main-detail">{{ message }}</div>
-</template>
-
+ <template>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="college"
+        label="院系"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="number"
+        label="教师编号"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="evaluate"
+        label="好评率">
+      </el-table-column>
+    </el-table>
+  </template>
 <script>
-export default {
-  name: "TSstudent",
-  data() {
-    return {
-      message: "这是教师信息默认页"
-    };
-  }
-};
-</script>
+ export default {
+      data() {
+        return {
+          tableData: [{
+            college: '软件',
+            number:'1',
+            name: '李小龙',
+            evaluate: '90%'
+          }, {
+            college: '软件',
+            number:'2',
+            name: '李小龙',
+            evaluate: '90%'
+          },{
+            college: '软件',
+            number:'3',
+            name: '李小龙',
+            evaluate: '90%'
+          },{
+            college: '软件',
+            number:'4',
+            name: '李小龙',
+            evaluate: '90%'
+          },]
+        }
+      }
+    }
+  </script>
