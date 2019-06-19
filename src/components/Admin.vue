1494840d0e9f615be68f34351f5d568b21c602fd
yjr2268152113 <2268152113@qq.com>
2019-06-19 14:27:52 +0800

    6.19

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="main-detail">{{ message }}</div>
+  <div class="main-detail">
+    <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560935064445&di=2587de5736a5de3e6151a8d11ffeac24&imgtype=0&src=http%3A%2F%2Fhiphotos.baidu.com%2Flbsugc%2Fpic%2Fitem%2Fb219ebc4b74543a95176e6c31c178a82b901144b.jpg" alt="" srcset="">
+  </div>
 </template>
 
 <script>
@@ -12,3 +14,16 @@ export default {
   }
 };
 </script>
+<style>
+ .main-detail{
+   width: 1150px;
+   height: 540px;
+ }
+ .main-detail img{
+   height: 100%;
+   width: 100%;
+   background-repeat:no-repeat;
+   background-size:100%
+ }
+
+</style>
